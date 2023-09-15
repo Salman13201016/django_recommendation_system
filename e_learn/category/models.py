554af8862f5f03e2d1cd3d684a1527ff393398c4
salman <salmanmdsultan@gmail.com>
2023-09-15 22:36:09 +0600

    category migration done

--- e_learn/category/models.py
+++ e_learn/category/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
+class categories(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 500)
+
 # Create your models here.
