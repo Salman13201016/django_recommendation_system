526a879e33ec1865e372f18a91f889976c16de3d
salman <salmanmdsultan@gmail.com>
2023-09-15 22:28:44 +0600

    db config done

--- e_learn/e_learn/settings.py
+++ e_learn/e_learn/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "e_learn.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "innovative_elearn",
+        "USER":"root",
+        "PASSWORD": "",
+        "HOST":"localhost",
+        "PORT":3306
     }
 }
 
