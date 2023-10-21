fa9e7bb1642bb77a2075d3e1db111a8935f3861f
salman <salmanmdsultan@gmail.com>
2023-10-21 23:57:41 +0600

    template error in home.html

--- e_learn/userend/views.py
+++ e_learn/userend/views.py
@@ -10,12 +10,20 @@ def home(request):
     new_course1  = courses.objects.order_by('-pk')
     feature_data = ''
     course_data=''
-    feature_product1  = courses.objects.filter('cat_id_id=2')
+    feature_product1  = courses.objects.filter(cat_id_id=2)
+    print(len(feature_product1))
+    print(len(new_course1))
     discount_fee_list = []
     discount_fee_list2 = []
+    course_status = True
+    feature_status = True
+
 
     if(len(feature_product1)>1):
-        feature_product  = courses.objects.filter('cat_id_id=2').order_by('-pk')[:len(feature_product1)]
+        if(len(feature_product1)<=4):
+            feature_product  = courses.objects.filter(cat_id_id=2).order_by('-pk')[:len(feature_product1)]
+        else:
+            feature_product  = courses.objects.filter(cat_id_id=2).order_by('-pk')[:4]
         # new_course  = courses.objects.filter('cat_id_id=2').order_by('-pk')[:len(new_course1)]
         for i in feature_product:
             discount_fee = i.fee - ((i.discount * i.fee)/100)
@@ -24,33 +32,40 @@ def home(request):
             # discount_fee_list2.append(discount_fee2)
         # course_data = zip_longest(new_course, discount_fee_list)
         feature_data = zip_longest(feature_product, discount_fee_list)
+        feature_status = True
     if(len(new_course1)>1):
         
-        new_course  = courses.objects.order_by('-pk')[:len(new_course1)]
+        
+        if(len(feature_product1)<=4):
+            new_course  = courses.objects.order_by('-pk')[:len(new_course1)]
+        else:
+            new_course  = courses.objects.order_by('-pk')[:4]
         for i in new_course:
             discount_fee = i.fee - ((i.discount * i.fee)/100)
             
             discount_fee_list2.append(discount_fee)
 
         course_data = zip_longest(new_course, discount_fee_list2)
+        course_status = True
         
 
-    elif(len(feature_product1)==1 and len(new_course1)==0):
+    if(len(feature_product1)==1):
         feature_data = feature_product1
-        course_data = False
-    elif(len(feature_product1)==0 and len(new_course1)==1):
-        feature_data = False
+    elif(len(new_course1)==1):
         course_data = new_course1
     elif(len(feature_product1)==1 and len(new_course1)==1):
         feature_data = feature_product1
         course_data = new_course1
-    else:
-        feature_data = False
-        course_data = False
 
     
+    # c_Data = list(course_data)
+    # f_data = list(feature_data)
+    if(len(new_course1)==0):
+        course_status = False
+    if(len(feature_product1)==0):
+        feature_status = False
     
-    all_data = {'cats':cat_obj,'new_course':course_data,'feature_data':feature_data}
+    all_data = {'cats':cat_obj,'new_course':course_data,'feature_data':feature_data,'course_status':course_status,'feature_status':feature_status}
     
     return render(request,'user/home.html',all_data)
 
